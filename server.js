5d7f43994ad5ef03a1445ae31146800298a546d9
mamingyu <imamingyu@gmail.com>
2017-12-12 16:02:14 +0800

    simplify

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.post('/api', function(request, response){
 });
 
 // Start server
-app.listen(3000);
-console.log('API is running on port 3000');
+app.listen(8080);
+console.log('API is running on port 8080');
